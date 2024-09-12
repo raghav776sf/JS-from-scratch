9d39ca7501daa9604e66971ba180dac72d8b855a
Anurag Kesarwani <raghavkesarwani766@gmail.com>
2024-09-12 19:29:18 +0000

    concept of variables

---
+++ basics/variables.js
@@ -0,0 +1,12 @@
+const uniqueId = 2343;
+let emailId = "abcd.gmail.com";
+var phoneNum = 123456789;
+city = "NDLS";
+
+// uniqueId = 7;
+emailId = "qwer.com"
+phoneNum = 10000;
+city = "PRYJ"
+
+
+console.table([uniqueId, emailId, phoneNum, city]);
