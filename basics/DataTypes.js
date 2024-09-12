503f487e2897eb70b326bd50acacac7434e85d99
Anurag Kesarwani <raghavkesarwani766@gmail.com>
2024-09-12 19:56:15 +0000

    dataTypes

---
+++ basics/DataTypes.js
@@ -0,0 +1,21 @@
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+let age = 18
+let isLoggedIn = false
+let state;
+let name = "Anurag"
+let data = null;
+
+console.log(typeof name)
+console.log(typeof state)
+console.log(typeof isLoggedIn)
+console.log(typeof data)
+console.log(typeof age)
+// null is object
