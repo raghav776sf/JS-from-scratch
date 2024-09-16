535bf694f8044bf066be39bf1a64f3e932a65f6c
Anurag Kesarwani <raghavkesarwani766@gmail.com>
2024-09-16 19:01:22 +0000

    dataTypes summary

---
+++ basics/4-comparison.js
@@ -0,0 +1,21 @@
+
+// console.log(1>=2)
+// console.log(1<=2)
+// console.log(1!=2)
+// console.log(1==2)
+// console.log(1<2)
+
+// console.log("1"==2)
+// console.log("01"<2)
+
+// comparison and equality both works differently
+
+// console.log(null==0)
+// console.log(null>0)
+// console.log(null<0)
+// console.log(null<=0)
+
+console.log(undefined==0)
+console.log(undefined>0)
+console.log(undefined<0)
+console.log(undefined<=0)
---
+++ basics/5-datatypes-summary.js
@@ -0,0 +1,38 @@
+// how we are putting data into memory and how we can access stored data from memory data-types are divided into two parts
+
+//  Primitive : call by value
+
+//  7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+// console.log(id === anotherId);
+
+// const bigNumber = 3456543576654356754n
+
+
+
+// Reference (Non primitive) : call by referance
+
+// Array, Objects, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"];
+let myObj = {
+    name: "hitesh",
+    age: 22,
+}
+
+const myFunction = function(){
+    console.log("Hello world");
+}
+
+console.log(typeof heros);
+
