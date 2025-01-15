a532e3c710ad61dcdc43a6e8e8a82be835947e07
Anurag Kesarwani <raghavkesarwani766@gmail.com>
2025-01-15 12:17:36 +0000

    date concepts

---
+++ basics/8-dates.js
@@ -0,0 +1,13 @@
+const myDate = new Date();
+// console.log(myDate)
+// console.log(myDate.toString())
+// console.log(myDate.toDateString)
+// console.log(myDate.toISOString())
+// console.log(myDate.toJSON())
+// console.log(typeof myDate)
+// console.log(myDate.toLocaleDateString())
+// console.log(myDate.toLocaleString())
+// console.log(myDate.toLocaleTimeString())
+
+const customizeDate = new Date(1997, 8, 20);
+console.log(customizeDate.toDateString());
